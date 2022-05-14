953ddeb915b784c92854610e17c61a76358c38d5
Gaurav <gaurav1567@gmail.com>
2022-05-14 16:54:03 +0530

    two numbers were taken as input

---
+++ input_output/input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter two numbers" << endl;
+    int a;
+    cout << "a ";
+    cin >> a;
+    int b;
+    cout << "b ";
+    cin >> b;
+    int sum = a + b;
+    cout << "Sum of the two entered number is " << sum << endl;
+}
