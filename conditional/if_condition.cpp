33560e7068271199cba4f05faa8b90a6c45eebd8
Gaurav <gaurav1567@gmail.com>
2022-05-15 17:23:38 +0530

    Conditional Program

---
+++ conditional/if_condition.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a > b)
+    {
+        if (a > c)
+        {
+            cout << "A is greatest with value " << a << endl;
+        }
+        else
+        {
+            cout << "C is greatest with value " << c << endl;
+        }
+    }
+    else
+    {
+        if (b > c)
+        {
+            cout << "B is greatest with value " << b << endl;
+        }
+        else
+        {
+            cout << "C is greatest with value " << c << endl;
+        }
+    }
+}
