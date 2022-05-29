61e39da1044bf95b98030a5660cd32e0c40cd1b5
Gaurav <gaurav1567@gmail.com>
2022-05-29 17:48:13 +0530

    Important Algorithm file added

---
+++ bitWise.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("D:\\practice_code\\input.txt", "r", stdin);
+    freopen("D:\\practice_code\\output.txt", "w", stdout);
+#endif
+
+    cout << (1 << 1) << endl;
+    return 0;
+}
